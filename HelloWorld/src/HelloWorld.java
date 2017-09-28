ba1a3fae55f3e4d0f6b0a6ca476591c1a7eea8da
Administrator <Administrator@TALAP20.mesd.k12.ca.us>
2017-09-27 17:11:16 -0700

    i love my leaf

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author Administrator
+ * Sept 27 2017 Joman & Eric
+ */
+//This program is to display "Hello World!" on console
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
